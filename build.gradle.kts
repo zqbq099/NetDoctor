3308d429c977886c200137b74aec8dea1fdba587
zqbq099 <zqbq099@gmail.com>
2026-04-15 01:00:24 +0300

    Remove clean task registration
    Removed the clean task registration from build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,3 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
